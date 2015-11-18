0f27c4f39010d94988dc9854131a5aad9dcd19ed
雷明林 <minglinlei@angejia.com>
2015-11-18 11:26:10 +0800

    modify lml.php url

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ ini_set('session.cookie_domain', APP_DOMAIN);
 session_start();
 function getRemoteLmlPhp(){
 	$cache_filename = 'lml.min.php';
-	$remotelib = 'http://pro8091d8.pic12.websiteonline.cn/upload/lmlphp-release-2014-08-27-v1.txt';
+	$remotelib = 'http://git.oschina.net/leiminglin/LMLPHP/raw/master/lml.min.php';
 	if( file_exists( $cache_filename ) ) {
 		$cachemtime = filemtime($cache_filename);
 		if( $cachemtime + 86400 > time() ){
@@ -39,7 +39,7 @@ $lastRouter = array('last');
 $domain = array(
 	APP_DOMAIN => array(
 		'blog' => array(
-			'/^(?:\/index\.php)?\/p\/([1-9][\d]*)/i'=>array( 
+			'/^(?:\/index\.php)?\/p\/([1-9][\d]*)/i'=>array(
 				'param'=>array('pid'),
 				'm'=>'index'
 			)
