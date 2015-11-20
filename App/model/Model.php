59f015091af0eda5ddc7428b2a145686c9f0a8fb
雷明林 <minglinlei@angejia.com>
2015-11-20 18:48:31 +0800

    use pdo if support

--- App/model/Model.php
+++ App/model/Model.php
@@ -3,11 +3,15 @@ abstract class Model{
 	public $dbconfig;
 	public $db;
 	public $dbPrefix;
-	
+
 	public function __construct(){
 		$dbconfig = $GLOBALS['dbconfig'];
 		$this->dbconfig = $dbconfig;
-		$this->db = Mysql::getInstance($dbconfig);
+		if (extension_loaded('pdo_mysql') && extension_loaded('PDO')) {
+			$this->db = MysqlPdoEnhance::getInstance($dbconfig);
+		} else {
+			$this->db = Mysql::getInstance($dbconfig);
+		}
 		$this->dbPrefix = $dbconfig['dbprefix'];
 	}
 }
