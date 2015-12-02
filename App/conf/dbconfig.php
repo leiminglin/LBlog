9d64a67ac51605315a0ed31b4c6d9e01e7c223d2
雷明林 <minglinlei@angejia.com>
2015-12-02 10:08:00 +0800

    add config sample

--- App/conf/dbconfig.php
+++ App/conf/dbconfig.php
@@ -1,1 +1,10 @@
-<?php return $dbconfig=array();
+<?php return $dbconfig=array(
+//	'hostname' => 'localhost',
+//	'hostport' => '3306',
+//	'username' => 'root',
+//	'password' => 'root',
+//	'database' => 'lblog',
+//	'charset' => 'utf8',
+//	'persist' => false,
+//	'dbprefix' => 'lblog_',
+);
