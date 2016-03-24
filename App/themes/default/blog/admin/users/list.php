44be85439b57a008ca8a1a88f2b1e96015e63615
may <leiminglin@126.com>
2016-03-24 23:34:36 +0800

    -_-

---
+++ App/themes/default/blog/admin/users/list.php
@@ -0,0 +1,28 @@
+
+
+<table>
+<tr>
+<th>ID</th>
+<th>Email</th>
+<th>NickName</th>
+<th>Source</th>
+<th>CreateTime</th>
+<th>Action</th>
+</tr>
+<?php foreach ($rs as $k=>$v){?>
+<tr>
+<td><?php echo $v['id'];?></td>
+<td><?php echo $v['email'];?></td>
+<td><?php echo $v['nickname'];?></td>
+<td><?php echo $v['source'];?></td>
+<td><?php echo $v['createtime'];?></td>
+<td>
+<a href="javascript:void(0)" data-action="" data-id="<?php echo $v['id'];?>">Edit</a>
+</td>
+</tr>
+<?php }?>
+</table>
+<?php
+$data_action = 'lblog_admin_users_page';
+include DEFAULT_THEME_PATH.C_GROUP.'/admin/common/admin_page.php';
+?>
