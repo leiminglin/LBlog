56e5b3980e550a6069f7773bfc5fa2fe6d618268
may <leiminglin@126.com>
2016-04-21 23:42:02 +0800

    -_-

--- App/conf/siteconfig.php
+++ App/conf/siteconfig.php
@@ -1,4 +1,4 @@
 <?php
 define('SITE_NAME', htmlspecialchars(s('config', 'SITE_NAME')));
 define('LBLOGUSS', 'LBLOGUSS');
-define('LBLOGSALT', 'salt_need_install_to_generate');
+define('LBLOGSALT', s('config', 'LBLOGSALT'));
